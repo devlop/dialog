872e43caaee1d968a27f88dd221e734474c6abb0
Johan <johan@devlop.se>
2021-08-17 14:46:53 +0700

    first poc

--- README.md
+++ README.md
@@ -5,8 +5,10 @@
 
 # dialog
 
-Modern drop-in replacement for native dialog methods [alert](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert), 
-[confirm](https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm) and [prompt](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt).
+Modern drop-in replacement for native dialog methods 
+[alert](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert), 
+[confirm](https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm) and 
+[prompt](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt).
 
 # Installing
 
@@ -18,19 +20,20 @@ npm install @devlop-ab/dialog
 
 # Usage
 
-All methods are compatible with the standard dialog methods and accept the same arguments, plus an additional `options` argument to modify the dialog.
+All methods are compatible with the standard dialog methods and accept the same arguments, 
+plus an additional `options` argument to modify the dialog.
 
 ```javascript
 import { dialog } from '@devlop-ab/dialog';
 
 // alert (no return value)
-dialog.alert('This is an alert!');
+dialog.alert('Hello world!');
 
 // confirm (returns true or false)
-const result = dialog.confirm('Are you sure?');
+const result = dialog.confirm('Do you really want to leave?');
 
 // prompt (returns string or null)
-const result = dialog.prompt('Please enter your name', 'Johan');
+const result = dialog.prompt('Are you feeling lucky?', 'sure');
 ```
 
 It is also possible to import a single method instead of the whole dialog class.
@@ -40,3 +43,42 @@ import { alert } from '@devlop-ab/dialog';
 import { confirm } from '@devlop-ab/dialog';
 import { prompt } from '@devlop-ab/dialog';
 ```
+
+## Dialog options
+
+All dialog methods accept an (optional) options argument where you can do basic configuration changes.
+
+```javascript
+{
+    'title': 'Title of the dialog',
+}
+```
+
+It's not possible to change the design (such as hiding the header) using the options, 
+to change the design you need to customize the styles.
+
+## Styles 
+
+The javascript does not output any styles, you must include them in your css/sass build.
+
+**Using SASS** 
+
+```sass
+// to get the default theme.
+@import '@devlop-ab/dialog/css';
+
+// or choose another theme.
+@import '@devlop-ab/dialog/css/windows-xp';
+@import '@devlop-ab/dialog/css/pink';
+```
+
+**Using vanilla CSS** 
+
+Copy the css file you want to use from the `node_modules/@devlop-ab/dialog/css` directory and put it with your other assets.
+
+## Customize the design.
+
+It's easiest to customize the design by importing the theme you want to use and then 
+declaring your own css to change the parts you want to change.
+
+If you do major design changes it's recommended to copy the theme instead and changing what you want.
---
+++ src/js/index.ts
@@ -0,0 +1,393 @@
+'use strict';
+
+const template = `
+    <div data-dialog-backdrop>
+        <div data-dialog="{type}" tabindex="-1" role="dialog">
+            <header>
+                <span>{title}</span>
+                <button type="button" data-role="cancel"></button>
+            </header>
+            <section>
+                <span>{message}</span>
+            </section>
+            <section data-role="prompt-section">
+                <input type="text"
+                    value=""
+                >
+            </section>
+            <footer>
+                <button data-role="cancel">{cancelText}</button>
+                <button data-role="ok">{okText}</button>
+            </footer>
+        </div>
+    </div>
+`;
+
+// DialogOptionsInterface (abstract)
+//     title
+// AlertDialogOptionsInterface
+//     okText
+// ConfirmDialogOptionsInterface
+//     cancelText
+//     okText
+// PromptDialogOptionsInterface
+//     cancelText
+//     okText
+//     input : InputOptionsInterface
+// InputOptionsInterface
+//     type, required, placeholder, min, pattern and more...
+
+interface EventListenerReference {
+    type : string;
+    listener: EventListenerOrEventListenerObject;
+};
+
+type TabbableElement = HTMLAnchorElement | HTMLButtonElement | HTMLInputElement;
+
+const makeDialog = (
+    type : string,
+    message : string,
+    title : string,
+    cancelText : string,
+    okText : string,
+) : HTMLElement => {
+    let dialogHTML = template;
+
+    dialogHTML = dialogHTML.replace('{type}', type);
+    dialogHTML = dialogHTML.replace('{message}', message);
+    dialogHTML = dialogHTML.replace('{title}', title);
+    dialogHTML = dialogHTML.replace('{cancelText}', cancelText);
+    dialogHTML = dialogHTML.replace('{okText}', okText);
+
+    const div : HTMLElement = document.createElement('div');
+    div.insertAdjacentHTML('afterbegin', dialogHTML);
+
+    return div.firstElementChild as HTMLElement;
+};
+
+const makeAlertDialog = (
+    message : string,
+) : HTMLElement => {
+    const dialog = makeDialog(
+        'alert',
+        message,
+        '', // title
+        'Cancel', // not relevant here
+        'Ok',
+    );
+
+    // remove the input section
+    removeDialogInputSection(dialog);
+
+    // remove the cancel button
+    removeDialogCancelButton(dialog);
+
+    return dialog;
+};
+
+const makeConfirmDialog = (
+    message : string,
+) : HTMLElement => {
+    const dialog = makeDialog(
+        'confirm',
+        message,
+        '', // title
+        'Cancel',
+        'Ok',
+    );
+
+    // remove the input section
+    removeDialogInputSection(dialog);
+
+    return dialog;
+};
+
+const makePromptDialog = (
+    message : string,
+) : HTMLElement => {
+    const dialog = makeDialog(
+        'prompt',
+        message,
+        '', // title
+        'Cancel',
+        'Ok',
+    );
+
+    return dialog;
+};
+
+const removeDialogInputSection = (dialog : HTMLElement) : void => {
+    dialog.querySelector('section[data-role="prompt-section"]')?.remove();
+};
+
+const removeDialogCancelButton = (dialog : HTMLElement) : void => {
+    dialog.querySelector('footer > button[data-role="cancel"]')?.remove();
+};
+
+const documentEventListeners : Array<EventListenerReference> = [];
+
+const registerEventListeners = (
+    dialog : HTMLElement,
+    okCallback : () => void,
+    cancelCallback : () => void,
+) : void => {
+    const backdrop : HTMLElement | null = 'dialogBackdrop' in dialog.dataset
+        ? dialog // the backdrop is the outermost element
+        : dialog.querySelector('[data-dialog-backdrop]'); // find the backdrop as a child
+
+    // close the dialog (cancel) when the backdrop is clicked
+    backdrop?.addEventListener('click', (event : MouseEvent) : void => {
+        if (event.target !== backdrop) {
+            return;
+        }
+
+        cancelCallback.apply(null);
+    });
+
+    // close the dialog (cancel) when the escape key is pressed
+    registerDocumentEventListener('keydown', (event : KeyboardEvent) : void => {
+        if (event.key !== 'Escape') {
+            return;
+        }
+
+        cancelCallback.apply(null);
+    });
+
+    // close the dialog (cancel) when any cancel button is clicked
+    (dialog.querySelectorAll('[data-role="cancel"]') as NodeListOf<HTMLButtonElement>).forEach((cancelButton : HTMLButtonElement) : void => {
+        cancelButton.addEventListener('click', (event : MouseEvent) : void => {
+            cancelCallback.apply(null);
+        });
+    });
+
+    // close the dialog (ok) when any ok button is clicked
+    (dialog.querySelector('[data-role="ok"]') as HTMLButtonElement).addEventListener('click', (event : MouseEvent) : void => {
+        okCallback.apply(null);
+    });
+
+    // close the dialog (ok) if enter is pressed while input field is focused
+    const inputField : HTMLInputElement | null = getInputField(dialog);
+
+    if (inputField !== null) {
+        inputField.addEventListener('keydown', (event : KeyboardEvent) : void => {
+            if (event.key === 'Enter') {
+                okCallback.apply(null);
+            }
+        });
+    }
+
+    // wrap tab
+    registerTabTrap(dialog);
+};
+
+const registerDocumentEventListener = (type : string, listener : Function) : void => {
+    // @ts-ignore
+    document.addEventListener(type, listener);
+
+    documentEventListeners.push({
+        type: type,
+        // @ts-ignore
+        listener: listener,
+    });
+};
+
+const unregisterDocumentEventListeners = () : void => {
+    let eventListenerReference : EventListenerReference | undefined;
+
+    while (typeof (eventListenerReference = documentEventListeners.shift()) !== 'undefined') {
+        document.removeEventListener(eventListenerReference.type, eventListenerReference.listener);
+    }
+};
+
+const registerTabTrap = (dialog : HTMLElement) : void => {
+    const tabbableElements = Array.from(dialog.querySelectorAll('a, button, input') as NodeListOf<TabbableElement>)
+        .filter((tabbableElement : TabbableElement) : boolean => {
+            const computedStyle : CSSStyleDeclaration = window.getComputedStyle(tabbableElement);
+
+            if (computedStyle.display === 'none') {
+                return false;
+            }
+
+            if (computedStyle.visibility === 'hidden') {
+                return false;
+            }
+
+            // @ts-ignore
+            if (['button', 'input'].includes(tabbableElement.tagName) && tabbableElement.disabled === true) {
+                return false;
+            }
+
+            return true;
+        });
+
+    if (tabbableElements.length === 0) {
+        return;
+    }
+
+    const firstTabbableElement : TabbableElement = tabbableElements[0];
+    const lastTabbableElement : TabbableElement = tabbableElements[tabbableElements.length - 1];
+
+    // if any elements outside the dialog is about to receive focus, snatch it back to the dialog
+    registerDocumentEventListener('focusin', (event : Event) : void => {
+        if (! dialog.contains(event.target as HTMLElement)) {
+            event.preventDefault();
+
+            firstTabbableElement.focus();
+        }
+    });
+
+    // @ts-ignore
+    firstTabbableElement.addEventListener('keydown', (event : KeyboardEvent) : void => {
+        // only wrap if shift + tab was pressed
+        const shouldWrap : boolean = event.key === 'Tab' && event.shiftKey;
+
+        if (shouldWrap) {
+            event.preventDefault();
+
+            // move focus back down to last input when "tabbing away" backwards from first element
+            lastTabbableElement.focus();
+        }
+    });
+
+    // @ts-ignore
+    lastTabbableElement.addEventListener('keydown', (event : KeyboardEvent) : void => {
+        // only wrap tab (without shift key) was pressed
+        const shouldWrap : boolean = event.key === 'Tab' && ! event.shiftKey;
+
+        if (shouldWrap) {
+            event.preventDefault();
+
+            // move focus back up to first input when "tabbing away" from last element
+            firstTabbableElement.focus();
+        }
+    });
+};
+
+const openDialog = (dialog : HTMLElement) : void => {
+    document.body.appendChild(dialog);
+};
+
+const closeDialog = (dialog : HTMLElement) : void => {
+    dialog.remove();
+
+    unregisterDocumentEventListeners();
+};
+
+const focusOkButton = (dialog : HTMLElement) : void => {
+    (dialog.querySelector('button[data-role="ok"]') as HTMLButtonElement).focus();
+};
+
+const getInputField = (dialog : HTMLElement) : HTMLInputElement | null => {
+    return dialog.querySelector('input') as HTMLInputElement | null;
+};
+
+const focusInputField = (dialog : HTMLElement) : void => {
+    getInputField(dialog)?.focus();
+};
+
+const setInputFieldValue = (dialog : HTMLElement, value : string) : void => {
+    const inputField = getInputField(dialog);
+
+    if (inputField !== null) {
+        inputField.value = value;
+    }
+};
+
+const getInputFieldValue = (dialog : HTMLElement) : string => {
+    return getInputField(dialog)?.value ?? '';
+};
+
+const alertDialog = (message? : string | undefined | null) : Promise<void> => {
+    return new Promise<void>((resolve, reject) : void => {
+        const dialog = makeAlertDialog(message ?? '');
+
+        openDialog(dialog);
+
+        focusOkButton(dialog);
+
+        registerEventListeners(
+            dialog,
+            // ok handler
+            () : void => {
+                closeDialog(dialog);
+
+                resolve();
+            },
+            // cancel handler
+            () : void => {
+                closeDialog(dialog);
+
+                resolve();
+            },
+        )
+    });
+};
+
+const confirmDialog = (message? : string | undefined | null) : Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) : void => {
+        const dialog = makeConfirmDialog(message ?? '');
+
+        openDialog(dialog);
+
+        focusOkButton(dialog);
+
+        registerEventListeners(
+            dialog,
+            // ok handler
+            () : void => {
+                closeDialog(dialog);
+
+                resolve(true);
+            },
+            // cancel handler
+            () : void => {
+                closeDialog(dialog);
+
+                resolve(false);
+            },
+        )
+    });
+};
+
+const promptDialog = (message? : string | undefined, _default? : string | undefined | null) : Promise<string | null> => {
+    return new Promise<string | null>((resolve, reject) : void => {
+        const dialog = makePromptDialog(message ?? '');
+
+        openDialog(dialog);
+
+        setInputFieldValue(dialog, _default ?? '');
+        focusInputField(dialog);
+
+        registerEventListeners(
+            dialog,
+            // ok handler
+            () : void => {
+                closeDialog(dialog);
+
+                const value : string = getInputFieldValue(dialog);
+
+                resolve(value);
+            },
+            // cancel handler
+            () : void => {
+                closeDialog(dialog);
+
+                resolve(null);
+            },
+        )
+    });
+};
+
+const dialog = {
+    'alert': alertDialog,
+    'confirm': confirmDialog,
+    'prompt': promptDialog,
+};
+
+export {
+    dialog as default,
+    dialog as dialog,
+    alertDialog as alert,
+    confirmDialog as confirm,
+    promptDialog as prompt,
+};
